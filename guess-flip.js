11ad68cc0c73d6f3e41116f88947e63a64b82c86
pranav23 <pranav23@live.unc.edu>
2022-04-29 03:06:25 -0400

    a02v6

---
+++ guess-flip.js
@@ -0,0 +1,18 @@
+import { flipACoin, coinFlip, countFlips } from "./modules/coin.mjs";
+import minimist from "minimist";
+
+const args = minimist(process.argv.slice(2))
+
+const call = args[call];
+
+if(call == "tails" || call == "heads"){
+
+    console.log(flipACoin(call));
+
+} 
+
+else{
+
+    console.log("Error: no inputs.");
+
+}
