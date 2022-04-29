34b68e4ad002de6a5f0840a78bab5e1866e07020
pranav23 <pranav23@live.unc.edu>
2022-04-29 03:44:10 -0400

    v17

--- guess-flip.js
+++ guess-flip.js
@@ -2,41 +2,30 @@ import { flipACoin, coinFlip, countFlips } from "./modules/coin.mjs";
 import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2))
-// args['call'];
-const call1 = args['call'] || null;
+args['call'];
+const call1 = args['call'];
 var flips = flipACoin(call1)
 
 
-// if(call1 == null || call1 == undefined) {
+if(call1 == null || call1 == undefined) {
     
-//     console.log("Error: no inputs")
-//     console.log('Usage: node guess-flip --call=[heads|tails]')
+    console.log("Error: no inputs")
+    console.log('Usage: node guess-flip --call=[heads|tails]')
 
-// } else if ( call1 != "tails" && call1 != "heads" ) {
+} else if ( call1 != "tails" && call1 != "heads" ) {
 
-//     console.log("Error: no inputs")
-//     console.log('Usage: node guess-flip --call=[heads|tails]')
+    console.log("Error: no inputs")
+    console.log('Usage: node guess-flip --call=[heads|tails]')
 
-// } else if (call1 == "tails" || call1 == "heads"){
+} else {
+
+    console.log(flips);
+
+} 
+
+// else {
 
 //     console.log(flips);
 
-// } 
+// }
 
-// // else {
-
-// //     console.log(flips);
-
-// // }
-
-if(call1 == undefined || call1 == null || (call1 != "heads" || call1 != "tails")) {
-
-    console.log("Error: no inputs\n Usage: node guess-flip --call=[heads||tails]");
-
-}
-
-else {
-    
-    console.log(flips);
-
-}
