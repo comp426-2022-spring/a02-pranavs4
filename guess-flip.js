3c6defc4845799b098d45e066f999fe9d2f0a4c4
pranav23 <pranav23@live.unc.edu>
2022-04-29 03:36:20 -0400

    v14

--- guess-flip.js
+++ guess-flip.js
@@ -2,22 +2,22 @@ import { flipACoin, coinFlip, countFlips } from "./modules/coin.mjs";
 import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2))
-args['call'];
-const call = args['call'];
-var flips = flipACoin(call)
+// args['call'];
+const call1 = args['call'] || null;
+var flips = flipACoin(call1)
 
 
-if(call == null || call == undefined) {
+if(call1 == null || call1 == undefined) {
     
     console.log("Error: no inputs")
     console.log('Usage: node guess-flip --call=[heads|tails]')
 
-} else if ( call != "tails" && call != "heads" ) {
+} else if ( call1 != "tails" && call1 != "heads" ) {
 
     console.log("Error: no inputs")
     console.log('Usage: node guess-flip --call=[heads|tails]')
 
-} else if(call == "tails" || call == "heads"){
+} else if (call1 == "tails" || call1 == "heads"){
 
     console.log(flips);
 
